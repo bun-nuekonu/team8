0088f70dccba8192ed66325dda89c63402e3b607
NagaseTAT <nagase121109@gmail.com>
2022-03-16 05:34:14 +0000

    データベースのモデル設定

--- ichidone/alarm/models.py
+++ ichidone/alarm/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.contrib.auth.models import UserManager, AbstractUser
 
-# Create your models here.
+class CustomUserManager(UserManager):
+    use_in_migrations = True
+
+    def _create_user(self, username, password, **extra_fields):
+        if not username:
+            raise ValueError('username must be set')
+
+        user = self.model(username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
+
+    def create_user(self, username, password=None, **extra_fields):
+        if not username:
+            raise ValueError('username must be set')
+
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+
+        return self._create_user(username, password, **extra_fields)
+
+    def create_superuser(self, username, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True')
+
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser')
+
+        return self._create_user(username, password)
+
+
+class Users(AbstractUser):
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.username
+
+
+class Quizzes(models.Model):
+    quiz = models.TextField(null=False)
+    choice1_answer = models.TextField(null=False)
+    choice2 = models.TextField(null=False)
+    choice3 = models.TextField(null=False)
+    choice4 = models.TextField(null=False)
+
+
+class Times(models.Model):
+    time = models.TextField(null=False)
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
--- ichidone/ichidone/settings.py
+++ ichidone/ichidone/settings.py
@@ -101,7 +101,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/4.0/topics/i18n/
 
@@ -123,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Userテーブルの設定
+AUTH_USER_MODEL = 'alarm.Users'
