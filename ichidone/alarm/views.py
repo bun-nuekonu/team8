a8ede90197c0e5d250c59dd5e7c27cea11a3c7b4
NagaseTAT <nagase121109@gmail.com>
2022-03-18 02:53:23 +0000

    フォルダの構造変更

--- ichidone/alarm/views.py
+++ ichidone/alarm/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, World!!")
+    return render(request, "alarm/index.html")
