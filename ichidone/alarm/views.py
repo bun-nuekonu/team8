fdab170a49b710991c068f60a3c29bd8f12031b0
miyatarou0226 <miyatarou0226@gmail.com>
2022-03-18 10:53:18 +0000

    テスト追加・宮本

--- ichidone/alarm/views.py
+++ ichidone/alarm/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello, World!")
+    
+    return render(request, "alarm/index.html", data)
+
+def login(request):
+    return render(request,)
+    
+def logout(request):
+    return render(request,)
