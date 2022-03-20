e94c06fac73dc62d2bc801d163b4013e6a3a0a65
miyatarou0226 <miyatarou0226@gmail.com>
2022-03-20 08:45:01 +0000

    index.htmlに時間表示のJSを書き込みました

--- ichidone/ichidone/urls.py
+++ ichidone/ichidone/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('alarm/', include('alarm.urls')),
     path('admin/', admin.site.urls),
     path('alarm/', include('allauth.urls')),
+    path('alarm/index/', include('alarm.urls'))
 ]
