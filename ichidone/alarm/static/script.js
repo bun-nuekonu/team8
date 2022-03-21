8f7caed01a80324eb2edaee380fa30267b3f143f
NagaseTAT <nagase121109@gmail.com>
2022-03-21 03:07:26 +0000

    script.jsの追加

---
+++ ichidone/alarm/static/script.js
@@ -0,0 +1,15 @@
+function getTime() {
+  var now = new Date();
+  var h = ('0' + now.getHours()).slice(-2);
+  var m = ('0' + now.getMinutes()).slice(-2);
+  var s = ('0' + now.getSeconds()).slice(-2);
+  nowtime = h + ":" + m + ":" + s;
+  var timeDisplay = document.getElementById('timeDisplay');
+  timeDisplay.style.fontSize = "220px";
+  timeDisplay.style.fontFamily = "Hannotate SC, arial, sans-serif";
+  timeDisplay.innerHTML = nowtime;
+}
+
+function strClock() {
+  setInterval("getTime();", 1000);
+}
